d4aa608e0ea946f39e73011a99e8fd3d3162ac67
Nicholas Martinez <nicholasmartinez@my.unt.edu>
2020-09-16 19:47:46 -0500

    vc-relax input

--- src/algorithm.py
+++ src/algorithm.py
@@ -3,14 +3,24 @@ from ase.calculators.espresso import Espresso
 
 pseudopotentials = { 'Mo': 'Mo_ONCV_PBE-1.0.oncvpsp.upf', 'S': 's_pbe_v1.4.uspp.F.UPF' }
 
-# build MoS2
+# build MoS2 electrode
+
+# prepare input file
+# include pseudos in dir
+inCONTROL = { 'calculation': 'vc-relax', 'outdir': './tmp', 'forc_conv_thr': 0.001 }
+inSYSTEM = { 'ecutrho' , 'ecutwfc', 'ibrav', 'celldm(1)', 'nat': 3, 'ntyp': 2, 'tot_charge': 0 } # tot_charge will change
+inELECTRONS = { 'conv_thr': 5e-9 }
+inIONS = { 'ion_dynamics': 'bfgs' }
+inCELL = { 'cell_dynamics': 'bfgs', 'cell_dofree' = '2Dxy' }
 
 infile = {}
 
-# build electrode
-calc = Espresso() # run relax
-read_espresso_out(_PW_TOTEN_) # read output total energy; returns slice -- use str_to_value() method
+# run relax calculation
+relax = Espresso()
 
+# run pw calculation
+pw = Espresso()
+read_espresso_out(_PW_TOTEN_) # read output total energy; returns slice -- use str_to_value() method
 
 '''minimization for dq'''
 # build electrode
