1bfb84b985c933add5620e3f49fd4733f5a89b7c
Nicholas Martinez <nicholasmartinez@my.unt.edu>
2020-09-19 21:04:27 -0500

    write pseudocode

---
+++ src/workflow.py
@@ -0,0 +1,17 @@
+#from aiida.engine import * # import functions, workfunctions, workchains
+#from aiida.orm import * # import data types
+
+# read cifs
+# run vc-relax
+# add hydrogen
+
+#@function
+#def do_something():
+
+#@function
+#def do_something_else():
+
+#@workfunction
+#def do_multiple_things()
+#    def do_something()
+#    def do_something_else()
