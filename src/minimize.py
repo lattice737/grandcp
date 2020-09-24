4b25ce9aabb2c48df51ee13cc62a8d0fc11c74f0
Nicholas Martinez <nicholasmartinez@my.unt.edu>
2020-09-24 10:43:46 -0500

    initiate loops, fix attempts

--- src/minimize.py
+++ src/minimize.py
@@ -3,53 +3,54 @@ import json # read pseudos, ecutwfc, ecutrho
 from ase.io import read, write, espresso
 from ase import Atoms
 
-#for q in potentials: # variable charge
-#for n in hydrogens: # variable hydrogen atoms
-
-
 '''read cif -- working'''
 cif = read('./unitcell.cif') # update to receive input; set to MoS2 sample
 
-# make primitive unit cell -- maybe Atoms or geometry tools
+# check/make primitive unit cell -- try Atoms or geometry tools
 # make 2D supercell -- use ase.build.supercells
 
-'''display unit cell'''
+'''display unit cell -- working'''
 cifstr = cif.get_chemical_formula(mode='reduce')
 write(f"{cifstr}.png", cif, format='png', show_unit_cell=2, rotation='90x', scale=35)
 os.system(f"open {cifstr}.png")
-os.system(f"rm {cifstr}.png")
 
-'''get pseudos & cutoffs -- working'''
-sssp = json.load( open('./efficiency.json') )
+potentials = [0.0] # 0.0 when testing
+hydrogens = [0] # 0 when testing
 
-pseudodict = { atom: sssp[atom]['filename'] for atom in cif.get_chemical_symbols() }
-rholist = [ sssp[atom]['rho_cutoff'] for atom in cif.get_chemical_symbols() ]
-wfclist = [ sssp[atom]['cutoff'] for atom in cif.get_chemical_symbols() ]
-rho = min(rholist)
-wfc = min(wfclist)
+for q in potentials: # variable charge
+    
+    for n in hydrogens: # variable hydrogen atoms
 
+        # this line to add hydrogens -- no changes to cif/cell for now
 
-'''write qe input files -- working'''
-relaxinput = {
-    'control': { 'calculation': 'vc-relax', 'pseudo_dir': './pseudos/' },
-    'system': { 'ecutrho': rho, 'ecutwfc': wfc },
-    'electrons': { 'conv_thr': 5.E-3 },
-    'ions': { 'ion_dynamics': 'bfgs' },
-    'cell': { 'cell_dynamics': 'bfgs', 'cell_dofree': 'all' }
-    }
+        '''get pseudos & cutoffs -- working'''
+        sssp = json.load( open('./efficiency.json') )
+        pseudodict = { atom: sssp[atom]['filename'] for atom in cif.get_chemical_symbols() }
+        rho = min([ sssp[atom]['rho_cutoff'] for atom in cif.get_chemical_symbols() ])
+        wfc = min([ sssp[atom]['cutoff'] for atom in cif.get_chemical_symbols() ])
 
-n = 0 # remove when loops scripted
-pwin = espresso.write_espresso_in( open(f"cif{n}.in",'w'), cif, relaxinput, pseudopotentials=pseudodict )
+        '''write qe input files -- working'''
+        relaxinput = {
+            'control': { 'calculation': 'vc-relax', 'pseudo_dir': './pseudos/' },
+            'system': { 'ecutrho': rho, 'ecutwfc': wfc, 'tot_charge': q },
+            'electrons': { 'conv_thr': 5.E-3 },
+            'ions': { 'ion_dynamics': 'bfgs' },
+            'cell': { 'cell_dynamics': 'bfgs', 'cell_dofree': 'all' }
+            }
 
+        espresso.write_espresso_in( open(f"cif{n}.in",'w'), cif, relaxinput, pseudopotentials=pseudodict )
 
-'''run vc-relax -- debugging'''
-pw = '/Users/nicholas/Desktop/qe/bin/pw.x'
-# infile, outfile
-#os.system( f"{pw} < cif0.in > cif0.out" ) # exception raised: 'charge is wrong; smearing needed' ??
+    '''run vc-relax -- debugging'''
+    pw = '/Users/nicholas/Desktop/qe/bin/pw.x' # queue to slurm when script complete
+    #os.system( f"{pw} < cif{n}.in > cif{n}.out" ) # exception raised for H=1: 'charge is wrong; smearing needed' ??
 
-#etot = espresso.read_espresso_out(open('cif0.out'), index=6) # read total energy; RETURNS OBJECT -- NEED ATTRIBUTE
-#print(etot)
-
+    '''get output total energy -- ???'''
+    output = list( espresso.read_espresso_out('./cif0.out', index=slice(None), results_required=True) )
+    print(dir(output[-1].calc))
+    #for i in output[-1].calc.results.keys:
+    #    print(i)
+    
+'''remove temporary files'''
 
 '''minimization for dq'''
 # build electrode (NH = 0) # in progress
