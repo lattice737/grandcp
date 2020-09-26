1975fdeb8756274ae3d1d3b9d5177acd35eaf49a
Nicholas Martinez <nicholasmartinez@my.unt.edu>
2020-09-26 16:58:48 -0500

    electrode creation, output reading, etc

--- src/minimize.py
+++ src/minimize.py
@@ -1,21 +1,19 @@
 import os
 import json # read pseudos, ecutwfc, ecutrho
 from ase.io import read, write, espresso
+from ase.spacegroup import *
 from ase import Atoms
 
 '''read cif -- working'''
 cif = read('./unitcell.cif') # update to receive input; set to MoS2 sample
-
-# check/make primitive unit cell -- try Atoms or geometry tools
-# make 2D supercell -- use ase.build.supercells
-
-'''display supercell -- displays unit cell'''
-#write(f"{cifstr}.png", cif, format='png', show_unit_cell=2, rotation='90x', scale=35)
-#os.system(f"open {cifstr}.png")
-
-'''make separate directory'''
 cifstr = cif.get_chemical_formula(mode='reduce')
 
+# check/make primitive unit cell -- try Atoms or geometry tools
+
+'''build 2x2 supercell -- working'''
+electrode = crystal(cif, size=(2,2,1))
+write(f"{cifstr}.png", electrode, format='png', show_unit_cell=2, rotation='5x,30y,90z', scale=35) # electrode image
+'''simple check for existing files -- working'''
 if os.path.exists(cifstr):
     overwrite = input(f"{cifstr} already exists. Overwrite? (y/n): ")
     if overwrite != 'y':
@@ -23,20 +21,23 @@ if os.path.exists(cifstr):
         quit()
 else:
     os.mkdir(cifstr) # make new dir if unit cell dir does not exist
-
 os.chdir(f"./{cifstr}")
 
+'''create input files & store energy results -- in progress'''
 potentials = [0.0] # 0.0 when testing
 hydrogens = [0] # use hydrogens = int() for final script; link to unit cell surface in the future
+energies = {}
 
 for i,q in enumerate(potentials): # variable charge
     
     if not os.path.exists(f"Q{i}"): os.mkdir(f"Q{i}") # make new dir if charge dir does not exist
     os.chdir(f"./Q{i}")
-
+    
     for n in hydrogens: # change hydrogens to range(hydrogens+1) in final script
 
-        # this line to add hydrogens -- no changes to cif/cell for now
+        # this line to add hydrogens -- no changes to cell for now
+        
+        energies[f"Q{i}"] = {f"H{n}": 0} # {energies: {charge: {n-hydrogens: OUTPUT}}}
 
         '''get pseudos & cutoffs -- working'''
         sssp = json.load( open('../../efficiency.json') )
@@ -47,33 +48,32 @@ for i,q in enumerate(potentials): # variable charge
         '''write qe input files -- working'''
         relaxinput = {
             'control': { 'calculation': 'vc-relax', 'pseudo_dir': '../../pseudos/' },
-            'system': { 'ecutrho': rho, 'ecutwfc': wfc, 'tot_charge': q },
+            'system': { 'ecutrho': rho, 'ecutwfc': wfc, 'tot_charge': q, 'degauss': 0.1, 'smearing': 'mv' },
             'electrons': { 'conv_thr': 5.E-3 },
             'ions': { 'ion_dynamics': 'bfgs' },
             'cell': { 'cell_dynamics': 'bfgs', 'cell_dofree': 'all' }
             }
 
-        if n == 0:
-            fin, fout = f"{cifstr}.in", f"{cifstr}.out"
-        else:
-            fin, fout = f"{cifstr}H{n}.in", f"{cifstr}H{n}.out"
+        fin, fout = f"{cifstr}H{n}.in", f"{cifstr}H{n}.out"
         espresso.write_espresso_in( open(fin,'w'), cif, relaxinput, pseudopotentials=pseudodict )
 
         '''run vc-relax -- debugging'''
         pw = '/Users/nicholas/Desktop/qe/bin/pw.x' # update to take user input when script complete
         os.system( f"{pw} < {fin} > {fout}" ) # exception raised for H=1: 'charge is wrong; smearing needed' ??
-        output = list( espresso.read_espresso_out(fout, index=slice(None), results_required=True) )
         
-        #print(dir(output[-1].calc))
-        #for i in output[-1].calc.results.keys:
-        #    print(i)
+        with open(f"{fout}") as f:
+            for line in f:
+                strlist = line.strip().split()
+                if not strlist: continue
+                elif strlist[0] == '!':
+                    energies[f"Q{i}"][f"H{n}"] = float(strlist[-2])
 
     os.chdir('../') # back to unit cell directory
     
-'''remove temporary files'''
+print(energies)
 
 '''minimization for dq'''
-# build electrode (NH = 0) # in progress
+# build electrode (NH = 0) # DONE
 # create input files (NH = 1, 2, 3, 4) # DONE
 # perform relax in qe # DONE
 # take total energy from output # in progress
