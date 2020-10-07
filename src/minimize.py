6bbbb3d27f239d9091d4d2f96eb0b69d6e4e761d
Nicholas Martinez <nicholasmartinez@my.unt.edu>
2020-10-06 19:36:50 -0500

    complete hydrogen addition routine

--- src/minimize.py
+++ src/minimize.py
@@ -1,8 +1,8 @@
 import os
-import sys
 import json # read pseudos, ecutwfc, ecutrho
 from ase.io import read, write, espresso
 from ase.spacegroup import *
+from ase.atom import Atom # append individual H atoms
 from ase import Atoms
 
 '''read cif -- working'''
@@ -30,35 +30,40 @@ wfc = min([ sssp[atom]['cutoff'] for atom in cif.get_chemical_symbols() ])
 '''build 2x2 supercell -- working'''
 electrode = crystal(cif, size=(2,2,1))
 write(f"{cifstr}.png", electrode, format='png', show_unit_cell=2, rotation='5x,30y,90z', scale=35) # electrode image
-zmin = min(electrode.get_positions()[:,2]) # max z-coordinate
-zmax = max(electrode.get_positions()[:,2]) # min z-coordinate
-top = [ o for o in electrode.get_positions() if o[2] == zmax ] # positions of top surface atoms
-bottom = [ o for o in electrode.get_positions() if o[2] == zmin ] # positions of bottom surface atoms
+zmin = min(electrode.get_positions()[:,2]) # max electrode z-coordinate; top surface
+zmax = max(electrode.get_positions()[:,2]) # min electrode z-coordinate; bottom surface
+
+'''create hydrogen set'''
+hydrogens = [0] # initialize with 0 for enumeration
+for o in electrode.get_positions():
+    if o[2] == zmin:
+        o[2] -= 1
+        hydrogens.append(o)
+    elif o[2] == zmax:
+        o[2] += 1
+        hydrogens.append(o)
 
 '''minimize routine -- in progress'''
 potentials = [0.0] # 0.0 when testing
-hydrogens = [0] # use hydrogens = int() for final script; link to unit cell surface in the future
 energies = {}
 
-for i,q in enumerate(potentials): # variable charge
+for i,q in enumerate(potentials): # i-charges with magnitude q
     
     if not os.path.exists(f"Q{i}"): os.mkdir(f"Q{i}") # make new dir if charge dir does not exist
     os.chdir(f"./Q{i}")
     
-    for n in hydrogens: # change hydrogens to range(hydrogens+1) in final script
+    for n,r in enumerate(hydrogens): # n-hydrogens with position r=(x,y,z)
 
         energies[f"Q{i}"] = {f"H{n}": 0} # {charge: {n-hydrogens: OUTPUT}}
 
-        '''add n-hydrogens to electrode'''
+        '''add n-hydrogens to electrode -- working'''
         if n > 0:
-        
-            pseudodict['H'] = sssp['H']['cutoff'] # add hydrogen pseudo
+            electrode.append(Atom(symbol='H',position=r))
+            pseudodict['H'] = sssp['H']['filename'] # add hydrogen pseudo
             if int(wfc) > 60: wfc = 60.0
             if int(rho) > 480: rho = 480.0
-            
-            # pop() positions with z +/-1 and add hydrogen to electrode or input
 
-        '''write qe input files -- working'''
+        '''write qe input files -- debugging'''
         relaxinput = {
             'control': { 'calculation': 'vc-relax', 'pseudo_dir': '../../src/pseudos/' },
             'system': { 'ecutrho': rho, 'ecutwfc': wfc, 'tot_charge': q, 'degauss': 0.1, 'smearing': 'mv' },
@@ -71,7 +76,6 @@ for i,q in enumerate(potentials): # variable charge
         espresso.write_espresso_in( open(fin,'w'), electrode, relaxinput, pseudopotentials=pseudodict )
 
         '''run vc-relax -- working'''
-        '''
         pw = '/Users/nicholas/espresso/qe/bin/pw.x' # update to take user input when script complete
         os.system( f"{pw} < {fin} > {fout}" ) # exception raised for H=1: 'charge is wrong; smearing needed' ??
         
@@ -81,7 +85,6 @@ for i,q in enumerate(potentials): # variable charge
                 if not strlist: continue
                 elif strlist[0] == '!':
                     energies[f"Q{i}"][f"H{n}"] = float(strlist[-2])
-        '''
 
     os.chdir('../') # back to unit cell directory
     
@@ -89,7 +92,7 @@ print(energies) # FOR TESTING
 
 '''minimization for dq'''
 # build electrode (NH = 0) # DONE
-# create input files (NH = 1, 2, 3, 4) # in progress, adding H to electrode
+# create input files (NH = 1, 2, 3, 4) # DONE
 # perform relax in qe # DONE
 # take total energy from output # DONE
 # take total H energy # in progress
